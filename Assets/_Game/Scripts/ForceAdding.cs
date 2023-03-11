e1b05799fc1174aff054d42620cb97082d1213c6
Etien <stepan.filippov@yandex.ru>
2023-03-11 11:26:01 +0500

    zapolnenie sily

--- Assets/_Game/Scripts/ForceAdding.cs
+++ Assets/_Game/Scripts/ForceAdding.cs
@@ -43,7 +43,8 @@ public class ForceAdding : MonoBehaviour
     public float GetForcePercent()
     {
         if (startTime == 0) return 0;
-        if (Time.time - startTime > MAX_TIME) return 1;
-        return (Time.time - startTime) / MAX_TIME;
+        float percent = (Time.time - startTime) / MAX_TIME;
+        if ((int)percent % 2 == 0) return percent - (int)percent;
+        return 1 - (percent - (int)percent);
     }
 }
