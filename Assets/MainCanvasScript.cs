d421e4e95f429bad1d18373f1916bb9d378aef12
re_nKOFF <lex.keivi@mail.ru>
2023-03-13 01:42:16 +0500

    icons + some hz cho

--- Assets/MainCanvasScript.cs
+++ Assets/MainCanvasScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MainCanvasScript : MonoBehaviour
 {
+    [SerializeField] private GameObject _obj;
+    
     void Start()
     {
         if (PlayerPrefs.GetInt("destroy") == 1)
         {
+            _obj.SetActive(true);
             gameObject.SetActive(false);
             Time.timeScale = 1;
             PlayerPrefs.SetInt("destroy", 0);
