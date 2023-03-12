53c22e86ac640beed8e55ecb94bc5d13af1ef181
Filippov Stepan <43007025+debebantur@users.noreply.github.com>
2023-03-12 19:53:15 +0500

    added double jump (#11)

--- Assets/_Game/Scripts/FlyControlling.cs
+++ Assets/_Game/Scripts/FlyControlling.cs
@@ -7,22 +7,43 @@ using UnityEngine;
 public class FlyControlling : MonoBehaviour
 {
     private Rigidbody physics;
-    [SerializeField] TMP_Text text;
+    private bool isDoubleJumpPressed = false;
+
+    private float startTime;
+    private const float MAX_TIME = 5;
 
     private void Start()
     {
         physics = gameObject.GetComponent<Rigidbody>();
     }
 
+    private void OnEnable()
+    {
+        isDoubleJumpPressed = false;
+    }
+
     void Update()
     {
-        bool phonePress = Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary);
-        text.text = ((phonePress || Input.GetMouseButton(0)) && physics.velocity.y <= 0).ToString();
+        //bool phonePress = Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary);
+        //if ((phonePress || Input.GetMouseButton(0)) && physics.velocity.y <= 0)
+        //{
+        //    physics.AddForce(new Vector3(0,0.34f,0f), ForceMode.Impulse);
+        //}
 
-        if ((phonePress || Input.GetMouseButton(0)) && physics.velocity.y <= 0)
+        bool phonePress = Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+        if ((phonePress || Input.GetMouseButtonDown(0)) && !isDoubleJumpPressed)
         {
-            text.text = "entered";
-            physics.AddForce(new Vector3(0,0.34f,0f), ForceMode.Impulse);
+            if(physics.velocity.y < 0)
+                physics.velocity = new Vector3(physics.velocity.x, 0f, physics.velocity.z);
+            physics.AddForce(new Vector3(0, 10f, 0f), ForceMode.Impulse);
+            isDoubleJumpPressed = true;
+        }
+
+        bool phoneReleased = Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended;
+        if ((phoneReleased || Input.GetMouseButtonUp(0)) && isDoubleJumpPressed && physics.velocity.y > 0)
+        {
+            physics.velocity = new Vector3(physics.velocity.x, physics.velocity.y / 2f, physics.velocity.z);
+            Debug.Log("cut the jump");
         }
     }
 
